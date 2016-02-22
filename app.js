b7b7d630e06c9a1bb4fe8a049ae8f7aad06654f7
Robert Gill <RobertGill@etech.net>
2016-02-22 20:52:40 +0000

    Added jade/bootstrap

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(request, response) {
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade');
 
+app.get('/', function(request, response) {
+  response.render('test');
 });
 
 app.use(express.static('public'));
